00379ae0975df5d5a761d362d099296cc444ecb9
chizo <filip.cierkosz@gmail.com>
2022-03-27 23:29:19 +0100

    The repo has been re-organized. The project is to have a baord generator soon.

---
+++ generator.py
@@ -0,0 +1,13 @@
+'''
+SUDOKU SOLVER PROJECT: Sudoku board generator.
+
+Date created:
+    03/2022
+
+Author:
+    Filip J. Cierkosz
+'''
+
+class SudokuGenerator:
+    def __init__(self):
+        pass
---
+++ solver.py
@@ -0,0 +1,38 @@
+'''
+SUDOKU SOLVER PROJECT: AI bot implementing the backtracking 
+                       algotrithm to solve sudoku.
+
+Date created:
+    12/2021
+
+Date modified:
+    03/2022
+
+Author:
+    Filip J. Cierkosz
+'''
+
+from time import time
+
+class Solver:
+    def __init__(self):
+        pass
+
+    def findEmptySpot():
+        pass
+
+    def checkIfValid():
+        pass
+
+    def solve():
+        pass
+
+    # Function 5: Set the timer.
+    def setTimer():
+        start = time()
+        return start
+
+    # Function 6: Stop the timer and return the time of execution.
+    def stopTimer(start):
+        stop = time()
+        print(f'Time of execution: {stop-start}\n')
